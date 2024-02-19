e4bffa8d8e7771fb5a6eb299ecae10d6ac187a0d
ElTinmar <martin.privat@hotmail.fr>
2024-02-19 18:47:13 +0100

    update

--- LED.py
+++ LED.py
@@ -1,1 +1,38 @@
-# Can I get away with arduino or lab jack ? check how fast that is 
+import u3
+
+registers = {
+    'DAC0': 5000,
+    'DAC1': 5002
+}
+
+d = u3.U3()
+d.writeRegister(registers["DAC0"], 0)
+d.writeRegister(registers["DAC0"], 3.0)
+
+class LabJackU3:
+
+    registers = {
+        'DAC0': 5000,
+        'DAC1': 5002
+    }
+        
+    def __init__(self) -> None:
+        self.device = u3.U3()
+
+    def write(self, register: str, val):
+        self.device.writeRegister(self.registers[register], val)
+
+    def read(self, register) -> float:
+        return self.device.readRegister()
+    
+
+class LEDDB1:
+
+    def on(self):
+        pass
+
+    def off(self):
+        pass
+
+    def pwm(self, duty_cycle: float, frequency: float):
+        pass
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # OptoDMD
 
 Control DMD device for patterned 1P optogenetics stimulation
+
+# Install LabJack USB driver
+
+```
+git clone https://github.com/labjack/exodriver.git
+cd exodriver/
+sudo ./install.sh   
+```
