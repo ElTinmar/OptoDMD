a132c838b960c6fb21169344f66aff7d3e168fc5
ElTinmar <martin.privat@hotmail.fr>
2024-02-19 15:32:58 +0100

    comment

---
+++ DMD.py
@@ -0,0 +1,9 @@
+# We are using the DLP Lightcrafter 4500
+# which does not support uploading patterns to onboard flash on the fly
+# instead, you can upload a new firmware with images via USB 1.0
+# which isn't great.
+# Since I don't need to switch between patterns super fast, I will
+# just use the Lightcrafter's video input to display patterns.
+ 
+# Open a borderless window on the DMD and use video display
+  
