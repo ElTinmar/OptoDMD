abb2d6155367b28f889787e4d103102db2008118
ElTinmar <martin.privat@hotmail.fr>
2024-02-20 13:35:38 +0100

    update

--- LED.py
+++ LED.py
@@ -1,85 +1,191 @@
+# for Arduino, you can use pyFirmata 
+# https://pyfirmata.readthedocs.io/en/latest/ 
+
+# LabJack Documentation can be found here:
 # https://github.com/labjack/LabJackPython/blob/master/Examples/workingWithModbus.py
 # https://labjack.com/pages/support?doc=%2Fsoftware-driver%2Fdirect-modbus-tcp%2Fud-modbus-old-deprecated%2F
+# https://files.labjack.com/datasheets/LabJack-U3-Datasheet.pdf
 
 import u3
+from pyfirmata import Arduino
 from typing import Protocol
-
-registers = {
-    'DAC0': 5000,
-    'DAC1': 5002
-}
-
-d = u3.U3()
-d.writeRegister(registers["DAC0"], 0)
-d.writeRegister(registers["DAC0"], 3.0)
 
 class DigitalAnalogIO(Protocol):
 
-    def digitalRead(self) -> bool:
+    def digitalRead(self, channel: int) -> float:
         ...
 
-    def digitalWrite(self, val: bool) -> None:
+    def digitalWrite(self, channel: int, val: bool):
         ...
 
-    def pwm(self, duty_cycle: float, frequency: float) -> None:
+    def pwm_configure(self, channel: int, duty_cycle: float, frequency: float) -> None:
+        ...
+
+    def pwm_start(self) -> None:
+        ...
+
+    def pwm_stop(self) -> None:
         ...
         
-    def analogRead(self) -> float:
+    def analogRead(self, channel: int) -> float:
         ...
 
-    def analogWrite(self, val: float) -> None:
+    def analogWrite(self, channel: int, val: float) -> None:
         ...
 
-class LabJackU3:
+    def close(self) -> None:
+        ...
 
-    registers = {
-        'DAC0': 5000,
-        'DAC1': 5002,
-        'TimerClockBase': 7000,
-        'TimerClockDiviser': 7002,
+class myArduino:
+
+    # PWM frequency is around 490Hz on most pins, 
+    # and 980Hz on pin 5 and 6
+
+    def __init__(self, board_id: str) -> None:
+        self.device = Arduino(board_id)
+        self.pwm_pin = None
+        self.dc_value = 0
+
+    def digitalRead(self, channel: int) -> float:
+        pin = self.device.get_pin(f'd:{channel}:i')
+        return pin.read()
+
+    def digitalWrite(self, channel: int, val: bool):
+        pin = self.device.get_pin(f'd:{channel}:o')
+        pin.write(val)
+
+    def pwm_configure(self, channel: int, duty_cycle: float, frequency: float) -> None:
+        Warning('PWM frequency was ignored')
+        self.dc_value = duty_cycle
+        self.pwm_pin = self.device.get_pin(f'd:{channel}:p')
+
+    def pwm_start(self) -> None:
+        self.pwm_pin.write(self.dc_value)
+
+    def pwm_stop(self) -> None:
+        self.pwm_pin.write(0)
+        
+    def analogRead(self, channel: int) -> float:
+        pin = self.device.get_pin(f'a:{channel}:i')
+        return pin.read()
+
+    def analogWrite(self, channel: int, val: float) -> None:
+        pin = self.device.get_pin(f'a:{channel}:o')
+        pin.write(val)
+
+    def close(self) -> None:
+        self.device.exit()
+
+class LabJackU3LV:
+    
+    # Analog outputs
+    # 10 bit resolution
+    # [0.04V, 4.95V]
+    DAC0 = 5000
+    DAC1 = 5002
+
+    # Digital Input/Output
+    DIO0 = 6000
+    DIO1 = 6001
+    DIO2 = 6002
+    DIO3 = 6003
+    DIO4 = 6004
+    DIO5 = 6005
+    DIO6 = 6006
+    DIO7 = 6007
+
+    # 12 bits resolution
+    # single-ended: [0V, 2.44V]
+    # differential: [-2.44V, 2.44V] 
+    # special: [0V, 3.6V] 
+    AIN0 = 0
+    AIN1 = 2
+    AIN2 = 4
+    AIN3 = 6
+    AIN4 = 7
+    AIN5 = 8
+    AIN6 = 9
+    AIN7 = 10
+
+    channels = {
+        'AnalogInput': [AIN0,AIN1,AIN2,AIN3,AIN4,AIN5,AIN6,AIN7],
+        'AnalogOutput': [DAC0,DAC1],
+        'DigitalInputOutput': [DIO0,DIO1,DIO2,DIO3,DIO4,DIO5,DIO6,DIO7]
     }
+
+    TIMER_CLOCK_BASE = 7000
+    TIMER_CLOCK_DIVISOR = 7002
+    NUM_TIMER_ENABLED = 50501
+    TIMER_PIN_OFFSET = 50500
+    TIMER_CONFIG = 7100
+    TIMER_MODE_16BIT = 0
+    TIMER_MODE_8BIT = 1
+
+    CLOCK: int = 48 # I'm only using the 48MHz clock
         
     def __init__(self) -> None:
         self.device = u3.U3()
+        self.timer_mode = 0
+        self.dc_value = 65535
 
-    def write(self, register: str, val):
-        self.device.writeRegister(self.registers[register], val)
+    def analogWrite(self, channel: int, val: float) -> None:
+        self.device.writeRegister(self.channels['AnalogOutput'][channel], val)
 
-    def read(self, register) -> float:
-        return self.device.readRegister()
+    def analogRead(self, channel: int) -> float:
+        return self.device.readRegister(self.channels['AnalogInput'][channel])
     
-    def pwm(self, duty_cycle: float, frequency: float):
-        # duty_cycle between zero and one
+    def digitalWrite(self, channel: int, val: bool):
+        self.device.writeRegister(self.channels['DigitalInputOutput'][channel], val)
 
-        timer_clock_base = 48
-        timer_clock_divisor = int( (timer_clock_base * 1e6)/(frequency * 65536) )
+    def digitalRead(self, channel: int) -> float:
+        return self.device.readRegister(self.channels['DigitalInputOutput'][channel])
+    
+    def pwm_configure(self, channel: int = 4, duty_cycle: float = 0.5, frequency: float = 732.42) -> None:
+
+        if not (0 <= duty_cycle <= 1):
+            raise ValueError('duty_cycle should be between 0 and 1')
+
+        if frequency > 187_500:
+            raise ValueError('max frequency at 48MHz is 187_500 Hz')
+        elif frequency < 2.861:
+            raise ValueError('min frequency at 48MHz is 2.861 Hz')
+         
+        if frequency > 732.42:
+            self.timer_mode = self.TIMER_MODE_8BIT
+            div = 2**8
+        else:
+            self.timer_mode = self.TIMER_MODE_16BIT
+            div = 2**16
+    
+        # divisor should be in the range 0-255, 0 corresponds to a divisor of 256
+        timer_clock_divisor = int( (self.CLOCK * 1e6)/(frequency * div) )
+        if timer_clock_divisor == 256: timer_clock_divisor = 0 
 
         # set the timer clock to be 48 MHz
-        d.writeRegister(7000, timer_clock_base)
+        self.device.writeRegister(self.TIMER_CLOCK_BASE, self.CLOCK)
 
         # set divisor of 15
-        d.writeRegister(7002, timer_clock_divisor)
+        self.device.writeRegister(self.TIMER_CLOCK_DIVISOR, timer_clock_divisor)
 
         # Pin offset (FIO) 
-        d.writeRegister(50500, 4) # not sure about that 
+        self.device.writeRegister(self.TIMER_PIN_OFFSET, channel) 
 
-        # enable 1 timer 
-        d.writeRegister(50501, 1)
-
-        # Configure the timer for PWM
-        d.writeRegister(7100, [0, 65535*duty_cycle]) # what does that do ? First 0 then duty cycle ?
+        # 16 bit value for duty cycle
+        self.dc_value = int(65535*duty_cycle)
     
+    def pwm_start(self) -> None:
 
-class LEDDB1:
+        # enable Timer0
+        self.device.writeRegister(self.NUM_TIMER_ENABLED, 1)
 
-    def __init__(self, DAIO: DigitalAnalogIO) -> None:
-        self.DAIO = DAIO
+        # Configure the timer for 16-bit PWM
+        self.device.writeRegister(self.TIMER_CONFIG, [self.timer_mode, self.dc_value]) 
 
-    def on(self):
-        self.DAIO.digitalWrite(True)
+    def pwm_stop(self) -> None:
 
-    def off(self):
-        self.DAIO.digitalWrite(False)
+        # disable Timer0 
+        self.device.writeRegister(self.NUM_TIMER_ENABLED, 0)
 
-    def pwm(self, duty_cycle: float, frequency: float):
-        pass
+    def close(self) -> None:
+
+        self.device.close()
--- README.md
+++ README.md
@@ -9,3 +9,10 @@ git clone https://github.com/labjack/exodriver.git
 cd exodriver/
 sudo ./install.sh   
 ```
+
+# Load Firmata Sketch on the Arduino
+
+The sketch is available in the Arduino IDE’s built-in examples. To open it, access the File menu, then Examples, followed by Firmata, and finally StandardFirmata.
+1. Plug the USB cable into the PC.  
+2. Select the appropriate board and port on the IDE.  
+3. Press Upload.  
