dfde2d3585a8d157880409c1edb182becbe27998
ElTinmar <martin.privat@hotmail.fr>
2024-02-20 22:05:47 +0100

    update

--- LED.py
+++ LED.py
@@ -1,5 +1,23 @@
 from daq import DigitalAnalogIO
 import time
+from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtWidgets import QSpinBox, QSlider, QListWidgetItem, QListWidget, QFileDialog, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QWidget
+from qt_widgets import NDarray_to_QPixmap,  LabeledSliderSpinBox, LabeledSpinBox
+from typing import Protocol, List
+
+class LEDDriver(Protocol):
+    
+    def set_intensity(self, intensity: float) -> None:
+        ...
+
+    def on(self) -> None:
+        ...
+
+    def off(self) -> None:
+        ...
+
+    def pulse(self, duration_ms: int) -> None:
+        ...
 
 class LEDD1B:
     '''Control a Thorlabs LED driver for optogenetics stimulation using PWM'''
@@ -46,3 +64,85 @@ class LEDD1B:
         self.DAIO.pwm_start()
         time.sleep(duration_ms/1000.0)
         self.DAIO.pwm_stop()
+
+
+class DriverWidget(QWidget):
+    
+    def __init__(self, driver: LEDDriver, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.driver = driver
+        self.declare_components()
+        self.layout_components()
+
+    def declare_components(self):
+
+        self.intensity_slider = LabeledSliderSpinBox(self)
+        self.intensity_slider.setText('intensity (%)')
+        self.intensity_slider.setRange(0, 100)
+        self.intensity_slider.setValue(50)
+        self.intensity_slider.valueChanged.connect(self.set_intensity)
+
+        self.on_button = QPushButton(self)
+        self.on_button.setText('ON')
+        self.on_button.clicked.connect(self.on)
+
+        self.off_button = QPushButton(self)
+        self.off_button.setText('OFF')
+        self.off_button.clicked.connect(self.off)
+
+        self.pulse_spinbox = LabeledSpinBox(self)
+        self.pulse_spinbox.setText('pulse duration (ms)')
+        self.pulse_spinbox.setRange(0, 100_000)
+        self.pulse_spinbox.setValue(1000)
+        
+        self.pulse_button = QPushButton(self)
+        self.pulse_button.setText('pulse')
+        self.pulse_button.clicked.connect(self.pulse)
+
+    def layout_components(self):
+         
+        main_layout = QHBoxLayout(self)
+        main_layout.addStretch()
+        main_layout.addWidget(self.intensity_slider)
+        main_layout.addWidget(self.on_button)
+        main_layout.addWidget(self.off_button)
+        main_layout.addWidget(self.pulse_spinbox)
+        main_layout.addWidget(self.pulse_button)
+        main_layout.addStretch()
+
+    def set_intensity(self, val: int):
+        self.driver.set_intensity(val/100)
+
+    def on(self):
+        self.driver.on()
+
+    def off(self):
+        self.driver.off()
+
+    def pulse(self):
+        self.driver.pulse(duration_ms=self.pulse_spinbox.value())
+
+
+class LEDWidget(QWidget):
+
+    def __init__(self, led_drivers: List[LEDDriver], *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        self.led_drivers = led_drivers
+        self.declare_components()
+        self.layout_components()
+
+    def declare_components(self):
+        
+        self.driver_widgets = []
+        for driver in self.led_drivers:
+            self.driver_widgets.append(DriverWidget(driver))
+
+    def layout_components(self):
+        
+        main_layout = QVBoxLayout(self)
+        main_layout.addStretch()
+        for w in self.driver_widgets:
+            main_layout.addWidget(w)
+        main_layout.addStretch()
--- test.py
+++ test.py
@@ -1,13 +1,22 @@
 from daq import LabJackU3LV
-from LED import LEDD1B
+from LED import LEDD1B, LEDWidget
+from PyQt5.QtWidgets import QApplication
+import sys
 
 daio = LabJackU3LV()
-led = LEDD1B(daio, pwm_channel=4)
+led1 = LEDD1B(daio, pwm_channel=4)
+led2 = LEDD1B(daio, pwm_channel=5)
 
-led.set_intensity(0.5)
-led.on()
-led.set_intensity(0.75)
 
-led.off()
+led1.set_intensity(0.5)
+led1.on()
+led1.set_intensity(0.75)
 
-led.pulse(duration_ms=1000)
+led1.off()
+
+led1.pulse(duration_ms=1000)
+
+app = QApplication(sys.argv)
+window = LEDWidget(led_drivers=[led1, led2])
+window.show()
+sys.exit(app.exec())
