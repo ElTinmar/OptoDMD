6da9983d403b8d861b99f97ca695b87efaf7aeb7
ElTinmar <martin.privat@hotmail.fr>
2024-02-20 22:58:08 +0100

    update

--- test.py
+++ test.py
@@ -3,19 +3,18 @@ from LED import LEDD1B, LEDWidget
 from PyQt5.QtWidgets import QApplication
 import sys
 
-daio = LabJackU3LV()
+#daio = LabJackU3LV()
 ardu = myArduino("/dev/ttyUSB0")
 led1 = LEDD1B(ardu, pwm_channel=11)
-led2 = LEDD1B(ardu, pwm_channel=13)
+led2 = LEDD1B(ardu, pwm_channel=5)
 
-
+'''
 led1.set_intensity(0.5)
 led1.on()
 led1.set_intensity(0.75)
-
 led1.off()
-
 led1.pulse(duration_ms=1000)
+'''
 
 app = QApplication(sys.argv)
 window = LEDWidget(led_drivers=[led1, led2])
