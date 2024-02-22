7bee2dfd013a5c0a1cfba2f4586ef53e2cfda41d
ElTinmar <martin.privat@hotmail.fr>
2024-02-22 14:05:11 +0100

    added ip

--- test_Microscope.py
+++ test_Microscope.py
@@ -2,7 +2,11 @@ from Microscope import TwoPhoton
 from PyQt5.QtWidgets import QApplication
 import sys
 
+o1_263 = "192.168.236.75"
+PORT = "5555"
+zmq_adress= "tcp://" + o1_263 + ":" + PORT
+
 app = QApplication(sys.argv)
-window = TwoPhoton(zmq_adress="tcp://localhost:5560")
+window = TwoPhoton(zmq_adress)
 window.show()
 app.exec()
