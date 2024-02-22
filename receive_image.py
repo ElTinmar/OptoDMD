b686c58f28628146f25b349753c115715b2ad462
ElTinmar <martin.privat@hotmail.fr>
2024-02-22 02:11:09 +0100

    remove file

--- receive_image.py
+++
@@ -1,8 +0,0 @@
-import zmq
-import numpy as np
-
-context = zmq.Context()
-socket = context.socket(zmq.PULL)
-socket.connect("tcp://localhost:5560")
-message = socket.recv()
-image = np.matrix(message.decode())
