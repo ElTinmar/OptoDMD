8369ab52fbd89daaf1684199b1a5ba7d5c6d11a5
ElTinmar <martin.privat@hotmail.fr>
2024-02-22 17:36:39 +0100

    add receiver

--- test_Microscope.py
+++ test_Microscope.py
@@ -1,12 +1,13 @@
-from Microscope import TwoPhoton
+from Microscope import TwoPhoton, ImageReceiver
 from PyQt5.QtWidgets import QApplication
 import sys
 
 o1_263 = "192.168.236.75"
 PORT = "5555"
-zmq_adress= "tcp://" + o1_263 + ":" + PORT
+zmq_address= "tcp://" + o1_263 + ":" + PORT
 
 app = QApplication(sys.argv)
-window = TwoPhoton(zmq_adress)
+receiver = ImageReceiver(zmq_address)
+window = TwoPhoton(receiver)
 window.show()
 app.exec()
