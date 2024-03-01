e9face8c63a79146f9174082b384985339fe0151
ElTinmar <martin.privat@hotmail.fr>
2024-03-01 17:43:05 +0100

    update

--- DrawMasks.py
+++ DrawMasks.py
@@ -98,8 +98,6 @@ class DrawPolyMask(QWidget):
         self.clear.clicked.connect(self.clear_masks)
 
         self.mask_frame = QFrame(self)
-        #self.mask_frame.setFrameStyle(QFrame.Panel)
-        
         self.scroll_area = QScrollArea()
         self.scroll_area.setWidget(self.mask_frame)
         self.scroll_area.setWidgetResizable(True)
