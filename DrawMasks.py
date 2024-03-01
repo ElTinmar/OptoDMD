20c9fc0ff34df4102ba0ed1e6791ed49b9d96394
ElTinmar <martin.privat@hotmail.fr>
2024-03-01 17:30:26 +0100

    scrollbar always on

--- DrawMasks.py
+++ DrawMasks.py
@@ -96,12 +96,12 @@ class DrawPolyMask(QWidget):
         self.clear.clicked.connect(self.clear_masks)
 
         self.mask_frame = QFrame(self)
-        #self.mask_frame.setFixedHeight(self.image.shape[0])
         self.mask_frame.setFrameStyle(QFrame.Panel)
         
         self.scroll_area = QScrollArea()
         self.scroll_area.setWidget(self.mask_frame)
         self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
 
     def layout_components(self):
 
