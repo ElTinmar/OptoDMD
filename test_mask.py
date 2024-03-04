f2e8e9913f3bcd18ce83a21845cfab355b00b58a
ElTinmar <martin.privat@hotmail.fr>
2024-03-04 12:42:48 +0100

    update

--- test_mask.py
+++ test_mask.py
@@ -1,12 +1,18 @@
-from DrawMasks import DrawPolyMask
+from DrawMasks import DrawPolyMask, Bla
 from PyQt5.QtWidgets import QApplication
 import sys
 import cv2
 
-image = cv2.imread('/home/martin/Code/alignment_tools/toy_data/image_00.jpg')
+image = cv2.imread('toy_data/image_00.jpg')
 
 app = QApplication(sys.argv)
-window = DrawPolyMask()
-window.set_image(image)
+cam = DrawPolyMask(image)
+dmd = DrawPolyMask(image)
+twop = DrawPolyMask(image)
+window = Bla(
+    dmd_mask_drawer=dmd, 
+    cam_mask_drawer=cam, 
+    twop_mask_drawer=twop
+)
 window.show()
 app.exec()
