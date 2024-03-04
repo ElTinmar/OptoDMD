64d355f531ca5dde5f1128fb160ab67026d7c23a
ElTinmar <martin.privat@hotmail.fr>
2024-03-04 18:01:52 +0100

    update

--- test_mask.py
+++ test_mask.py
@@ -1,18 +1,17 @@
-from DrawMasks import DrawPolyMask, Bla
+from DrawMasks_2 import DrawPolyMask, MaskManager
 from PyQt5.QtWidgets import QApplication
 import sys
 import cv2
+import numpy as np
 
 image = cv2.imread('toy_data/image_00.jpg')
-
+transformations = np.tile(np.eye(3), (3,3,1,1))
 app = QApplication(sys.argv)
+
 cam = DrawPolyMask(image)
 dmd = DrawPolyMask(image)
 twop = DrawPolyMask(image)
-window = Bla(
-    dmd_mask_drawer=dmd, 
-    cam_mask_drawer=cam, 
-    twop_mask_drawer=twop
-)
+window = MaskManager([cam, dmd, twop], ["Camera", "DMD", "Two Photon"], transformations)
 window.show()
+
 app.exec()
