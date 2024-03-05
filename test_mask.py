3faa77cebb66506df608cc763d40ae2fca523361
ElTinmar <martin.privat@hotmail.fr>
2024-03-05 01:01:37 +0100

    update

--- test_mask.py
+++ test_mask.py
@@ -9,28 +9,21 @@ import numpy as np
 image = cv2.imread('toy_data/image_00.jpg')
 transformations = np.tile(np.eye(3), (3,3,1,1))
 #transformations[0,1,0,0] = 2.0
-
 DMD_HEIGHT = 1140
 DMD_WIDTH = 920
 
 app = QApplication(sys.argv)
-
 webcam = OpenCV_Webcam()
 webcam_controls = CameraControl(webcam)
-
-#dmd_display = DMD(screen_num=1)
-
+dmd_display = DMD(screen_num=1)
 cam = DrawPolyMaskOpto(image)
 dmd = DrawPolyMaskOptoDMD(DMD_HEIGHT, DMD_WIDTH)
 twop = DrawPolyMaskOpto(image)
-
-#dmd.DMD_update.connect(dmd_display.update_image)
-
-webcam_controls.image_ready.connect(cam.set_image)
-webcam_controls.show()
-
 window = MaskManager([cam, dmd, twop], ["Camera", "DMD", "Two Photon"], transformations)
-window.show()
-cam.show()
 
+dmd.DMD_update.connect(dmd_display.update_image)
+webcam_controls.image_ready.connect(cam.set_image)
+
+webcam_controls.show()
+window.show()
 app.exec()
