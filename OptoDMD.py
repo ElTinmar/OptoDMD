75dba1d89ee056dbea5fece2d2553154b9fba92e
ElTinmar <martin.privat@hotmail.fr>
2024-03-05 14:18:27 +0100

    remove imports

--- OptoDMD.py
+++ OptoDMD.py
@@ -1,4 +1,4 @@
-from Microscope import TwoPhoton, ImageSender
+from Microscope import ImageSender
 from DrawMasks import  MaskManager, DrawPolyMaskOpto, DrawPolyMaskOptoDMD
 from daq import LabJackU3LV, myArduino
 from LED import LEDD1B, LEDWidget
@@ -6,28 +6,9 @@ from DMD import DMD
 from camera_tools import XimeaCamera, CameraControl
 from PyQt5.QtWidgets import QApplication
 from alignment_tools import AlignAffine2D
-from image_tools import ROISelectorWidget
 import sys
 import numpy as np
 
-class OptoDMD():
-    
-    def __init__(
-            self, 
-            camera: CameraControl,
-            two_photon: TwoPhoton,
-            led: LEDWidget,
-            dmd: DMD,
-            calibration_file: str
-        ) -> None:
-
-        self.camera = camera
-        self.two_photon = two_photon
-        self.led = led
-        self.dmd = dmd
-        self.calibration_file = calibration_file
-
-    
 if __name__ == "__main__":
 
     O1_263 = "192.168.236.75"
