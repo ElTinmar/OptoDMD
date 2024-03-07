de64e4b9496ade31e4a2a21968ca8de721dfefb6
ElTinmar <martin.privat@hotmail.fr>
2024-03-07 20:41:38 +0100

    delete tests

--- test_DMD.py
+++
@@ -1,9 +0,0 @@
-from DMD import DMD, ImageSender
-from PyQt5.QtWidgets import QApplication
-import sys
-
-app = QApplication(sys.argv)
-window = DMD(screen_num=1)
-window.show()
-sendr = ImageSender(window)
-app.exec()
