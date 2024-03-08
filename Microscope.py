3f70b71e3ad6ea26c239cbd57e67c793bc85d709
ElTinmar <martin.privat@hotmail.fr>
2024-03-08 20:07:22 +0100

    update

--- Microscope.py
+++ Microscope.py
@@ -11,19 +11,44 @@ def deserialize(message: str) -> NDArray:
     data = [r.split(',') for r in message.split(';')]
     return np.array(data, dtype = np.float32)
 
+class ScanImage:
+
+    def __init__(self, protocol: str, host: str, port: int) -> None:
+
+        self.context = zmq.Context()
+
+        # get images from scanimage
+        address_image = protocol + host + str(port)
+        self.socket_image = self.context.socket(zmq.PULL)
+        self.socket_image.connect(address_image)
+
+        # send commands to scanimage 
+        address_control = protocol + host + str(port + 1)
+        self.socket_control = self.context.socket(zmq.PUSH)
+        self.socket_control.bind(address_control)
+    
+    def get_image(self) -> NDArray:
+
+        message = self.socket_image.recv()
+        image = deserialize(message.decode())
+        return image
+    
+    def set_zoom(self, zoom: float):
+
+        # TODO check range
+        self.socket_control.send(zoom)
+
 class ImageSignal(QObject):
     # only QObject can emit signals, not QRunnable
     image_ready = pyqtSignal(np.ndarray)
 
 class ImageSender(QRunnable):
 
-    def __init__(self, zmq_adress, *args, **kwargs):
+    def __init__(self, scan_image: ScanImage, *args, **kwargs):
 
         super().__init__(*args, **kwargs)
         
-        self.context = zmq.Context()
-        self.socket = self.context.socket(zmq.PULL)
-        self.socket.connect(zmq_adress)
+        self.scan_image = scan_image
         self.signal = ImageSignal()
         self.keepgoing = True
     
@@ -32,8 +57,7 @@ class ImageSender(QRunnable):
 
     def run(self):
         while self.keepgoing:
-            message = self.socket.recv()
-            image = deserialize(message.decode())
+            image = self.scan_image.get_image()
             self.signal.image_ready.emit(image)
 
 class TwoPhoton(QWidget):
