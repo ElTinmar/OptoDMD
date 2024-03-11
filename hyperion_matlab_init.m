9bc80da1cadc3c5deae01c2cc7de3022a0c40f7f
ElTinmar <martin.privat@hotmail.fr>
2024-03-11 13:23:04 +0100

    commenting out

--- hyperion_matlab_init.m
+++ hyperion_matlab_init.m
@@ -1,11 +1,11 @@
-scanimage_folder='C:\Program Files\Vidrio\SI-Premium_2020.1.2_(2021-02-09)_bd806fcff6';
-optoDMD_folder='P:\Code\OptoDMD-main';
-zeromq_jar_path = 'P:\Code\OptoDMD-main\jeromq-0.6.0.jar';
+%scanimage_folder='C:\Program Files\Vidrio\SI-Premium_2020.1.2_(2021-02-09)_bd806fcff6';
+optoDMD_folder='C:\Users\martin.privat\Documents\OptoDMD';
+zeromq_jar_path = 'C:\Users\martin.privat\Documents\OptoDMD\jeromq-0.6.0.jar';
 zeromq_address = "tcp://*:5555";
 channel = 1;
 
 % add scanimage path
-addpath(genpath(scanimage_folder));
+%addpath(genpath(scanimage_folder));
 addpath(genpath(optoDMD_folder));
 
 % cd to optodmd
