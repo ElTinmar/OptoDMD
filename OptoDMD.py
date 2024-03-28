416e140e1df2b8872e61c49f1f5de66a7e929808
ElTinmar <martin.privat@hotmail.fr>
2024-03-28 15:47:40 +0100

    update

--- OptoDMD.py
+++ OptoDMD.py
@@ -19,13 +19,13 @@ if __name__ == "__main__":
     DMD_WIDTH = 912
 
     # labjack settings
-    USE_LABJACK = False # Only set to False for testing purposes on a machine without a labjack
+    USE_LABJACK = True # Only set to False for testing purposes on a machine without a labjack
     PWM_CHANNEL = 6
 
     # camera settings
-    XIMEA_CAMERA_ID = None # Only set to None for testing purposes on a machine without a Ximea camera
-    CAM_HEIGHT = 480    
-    CAM_WIDTH = 640
+    XIMEA_CAMERA_ID = 1 # Only set to None for testing purposes on a machine without a Ximea camera
+    CAM_HEIGHT = 2048    
+    CAM_WIDTH = 2048
 
     # microscope settings
     PROTOCOL = "tcp://"
@@ -65,10 +65,12 @@ if __name__ == "__main__":
         cam = OpenCV_Webcam(-1)
 
     camera_controls = CameraControl(cam)
+    # disable ROI controls 
     camera_controls.height_spinbox.setDisabled(True)
     camera_controls.width_spinbox.setDisabled(True)
     camera_controls.offsetX_spinbox.setDisabled(True)
     camera_controls.offsetY_spinbox.setDisabled(True)
+
     camera_controls.show()
 
     # Control LEDs
